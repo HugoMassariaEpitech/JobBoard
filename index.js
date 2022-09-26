673380ae5072c7484d89e65901ac6c9220d3f985
hugomassaria <hugo.massaria@epitech.eu>
2022-09-26 17:30:55 +0200

    First Commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 $.ajax({type:'GET', url:'Advertisements.php', data:"", dataType: 'json', success: function(data) {
-    // $(".LeftSide").append(`<div><h1>${data[0].advertisement_name}</h1><p>${data[0].advertisement_company}</p><p>${data[0].advertisement_location}</p><p>${data[0].advertisement_type}</p><p>${data[0].advertisement_description}</p></div>`);
+    for (const advertisement of data) {
+        console.log(advertisement);
+        $(".Scroll").append(`<div class="Element"><h3 class="Titre">${advertisement.advertisement_name}</h3><p class="Description">${advertisement.advertisement_description}</p><button class="mdc-button mdc-button--outlined mdc-button--icon-leading"><span class="mdc-button__ripple"></span><i class="material-icons mdc-button__icon" aria-hidden="true">read_more</i><span class="mdc-button__label">VOIR</span></button></div>`);
+    }
+    console.log(data);
 }, error: function() {
     console.log("Erreur");
 }});
