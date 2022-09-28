39c279e3074efeed4fa2ff88aa23fff8c888ccae
hugomassaria <hugo.massaria@epitech.eu>
2022-09-28 16:25:43 +0200

    Register Page Commit

--- register/src/register.js
+++ register/src/register.js
@@ -1,9 +1,25 @@
-$(".SendButton").click(addUser);
+$(".LogButton").click(changeFormToLog);
+
+function changeFormToLog() {
+    $(".FormElement:lt(5)").hide();
+    $(".LogButton").html("Inscription");
+    $(".LogButton").click(changeFormToRegister);
+}
+
+function changeFormToRegister() {
+    $(".FormElement:lt(5)").show();
+    $(".LogButton").html("LogIn");
+    $(".LogButton").click(changeFormToLog);
+}
 
 function addUser() {
-    $.ajax({type:"POST", url:"../../api/user/register.php", data:"user_email=hugo.massaria@google.com&user_password=Hugo", success: function(data) {
+    $.ajax({type:'POST', url:'api/user/register.php', data:`user_email=${}&user_password=${}&user_name=${}&user_firstname=${}&user_phone=${}&user_birthdate=${}&user_civility=${}`, success: function(data) {
         console.log(data);
     }, error: function() {
         console.log("Erreur");
     }});
+}
+
+function logUser() {
+
 }
